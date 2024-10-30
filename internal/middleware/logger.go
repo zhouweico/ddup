6aec5a4f4570f1792850fa0aeccfcfe857286f12
ZHOUWEI <zhouwei.will@gmail.com>
2024-10-30 17:37:16 +0800

    first commit

--- internal/middleware/logger.go
+++ internal/middleware/logger.go
@@ -11,19 +11,41 @@ func Logger() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		start := time.Now()
 		path := c.Request.URL.Path
+		raw := c.Request.URL.RawQuery
 		method := c.Request.Method
+
+		clientIP := c.ClientIP()
 
 		c.Next()
 
 		latency := time.Since(start)
 		statusCode := c.Writer.Status()
+		bodySize := c.Writer.Size()
 
-		if len(c.Errors) > 0 {
-			// 记录错误日志
-			c.Error(c.Errors.Last())
+		if raw != "" {
+			path = path + "?" + raw
 		}
 
-		// TODO: 使用你的日志库记录请求信息
-		log.Printf("[%d] %s %s %v", statusCode, method, path, latency)
+		var logFunc func(string, ...interface{})
+		if statusCode >= 500 {
+			logFunc = log.Printf
+		} else if statusCode >= 400 {
+			logFunc = log.Printf
+		} else {
+			logFunc = log.Printf
+		}
+
+		logFunc("[GIN] %s | %3d | %13v | %15s | %s | %d bytes",
+			method,
+			statusCode,
+			latency,
+			clientIP,
+			path,
+			bodySize,
+		)
+
+		if len(c.Errors) > 0 {
+			log.Printf("[ERROR] %v", c.Errors.String())
+		}
 	}
 }
