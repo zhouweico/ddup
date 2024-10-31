6d3a63fe51c8699d630ac7adeb4e4bfb8a46b842
ZHOUWEI <zhouwei.will@gmail.com>
2024-10-31 16:15:54 +0800

    first commit

---
+++ scripts/commit.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# 检查是否提供了提交信息
+if [ -z "$1" ]; then
+    echo "请提供提交信息"
+    echo "使用方法: ./scripts/commit.sh '提交信息'"
+    exit 1
+fi
+
+# 生成 swagger 文档
+echo "正在生成 Swagger 文档..."
+swag init -g cmd/api/main.go -o docs
+
+# 检查 swag 命令是否执行成功
+if [ $? -ne 0 ]; then
+    echo "Swagger 文档生成失败"
+    exit 1
+fi
+
+# 添加所有更改到暂存区
+git add .
+
+# 提交更改
+git commit -m "$1"
+
+# 检查提交是否成功
+if [ $? -ne 0 ]; then
+    echo "Git 提交失败"
+    exit 1
+fi
+
+echo "完成！"
+echo "- Swagger 文档已更新"
+echo "- 更改已提交: $1" 
